25df911a77d0075ff12b0265e7b0eb44b4f31c7a
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-28 17:49:42 +0530

    Initial Commit

---
+++ HelloRedux/HelloReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloReduxApp.swift
+//  HelloRedux
+//
+//  Created by Sajed Shaikh on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
