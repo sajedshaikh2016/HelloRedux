9354ebc0fd3d2ad575fe406125e2a26abaa8bccb
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-28 18:29:53 +0530

    Mapping global states to local props & Injecting an environment object and displaying results

--- HelloRedux/ContentView.swift
+++ HelloRedux/ContentView.swift
@@ -8,17 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject var store: Store
+    
+    struct Props {
+        let counter: Int
+        let onIncrement: () -> Void
+    }
+    
+    private func map(state: State) -> Props {
+        Props(counter: state.counter, onIncrement: {
+            store.dispatch(action: IncrementAction())
+        })
+    }
+    
     var body: some View {
+        let props = map(state: store.state)
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(props.counter)")
+            Button("Increment") {
+                props.onIncrement()
+            }
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    let store = Store(reducer: reducer)
+    return ContentView().environmentObject(store)
 }
--- HelloRedux/HelloReduxApp.swift
+++ HelloRedux/HelloReduxApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct HelloReduxApp: App {
+    
+    let store = Store(reducer: reducer)
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(store)
         }
     }
 }
